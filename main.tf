198f5c18fd34eec2b861c162d755798a854fc034
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2021-06-11 17:57:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 2.0"
     }
-  }
- required_version = ">= 0.14.9"
 }
-
+}
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "resource1" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Bharathi"
-  }
+resource "aws_s3_bucket" "bharathibucket" {
+  bucket =  "practice-bucket-using-terraform-script-bharathi"
 }
+
