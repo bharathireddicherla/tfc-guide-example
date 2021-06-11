da43fa158a93ea61ef60084d60a7e9b3f36e05ef
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2021-06-11 17:51:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
 provider "aws" {
-  region = var.aws_region
+  profile = "default"
+  region  = "us-west-2"
 }
 
-provider "random" {}
+resource "aws_instance" "resource1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  tags = {
+    Name = "Bharathi"
   }
 }
